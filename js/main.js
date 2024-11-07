eee2b5568272a9a6f512727d61ceeec893cbf39e
enzo <enzovcnt@gmail.com>
2024-11-07 23:31:57 +0100

    try js but dont work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("Hello World!");
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    const suffix = this.dataset.boxSizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
